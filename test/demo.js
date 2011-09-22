e699ca8cc12987dfcf2094f4b56ee7b41f311847
mckamey <mckamey@localhost>
2011-09-22 00:03:47 -0700

    adding interactive demo

---
+++ test/demo.js
@@ -0,0 +1,42 @@
+(function() {
+	function byId(id) {
+		return document.getElementById(id);
+	}
+
+	// initialize units
+	for (var key in countdown) {
+		if (countdown.hasOwnProperty(key)) {
+			var unit = byId("units-"+key.toLowerCase());
+			if (unit) {
+				unit.value = countdown[key];
+			}
+		}
+	}
+
+	function update() {
+		var units = ~countdown.ALL,
+			chx = byId('countdown-units').getElementsByTagName('input');
+
+		for (var i=0, count=chx.length; i<count; i++) {
+			if (chx[i].checked) {
+				units |= Number(chx[i].value);
+			}
+		}
+
+		var yyyy = Number(byId('year').value),
+			MM = Number(byId('month').value)-1,
+			dd = Number(byId('date').value),
+			HH = Number(byId('hours').value),
+			mm = Number(byId('minutes').value),
+			ss = Number(byId('seconds').value),
+			fff = Number(byId('milliseconds').value);
+
+		var start = new Date(yyyy, MM, dd, HH, mm, ss, fff),
+			ts = countdown.timespan(start, null, units);
+
+		byId('counter').innerHTML = ts.toHTML("strong") || "now";
+		byId('timespan').innerHTML = JSON.stringify(ts, null, "  ");
+	}
+//	update();
+	setInterval(update, 1000/30);
+})();
